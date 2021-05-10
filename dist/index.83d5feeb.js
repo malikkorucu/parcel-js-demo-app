7e2d378326cc022421ee1a98d6d1779403775446
Malik Korucu <malikkorucu@gmail.com>
2021-05-10 21:13:58 +0300

    first commit

---
+++ dist/index.83d5feeb.js
@@ -0,0 +1,2 @@
+!function(){var e=(0,("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequirec9f8)("1UVYD");console.log(e().default.main)}();
+//# sourceMappingURL=index.83d5feeb.js.map
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import classes from './main.scss'
+
+export default () => {
+  console.log(classes.main);
+};
